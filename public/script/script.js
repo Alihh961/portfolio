3ae8182691cd8a9c289a085f344cf3000788c071
Alihh961 <hajhassan.ali@outlook.com>
2024-05-16 01:41:38 +0200

    move welcome section to up on clicking go button

--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,53 @@
-console.log('script.js file works');
+document.addEventListener('DOMContentLoaded', ()=> {
+    console.log('script.js file works');
 
-window.onscroll = function() {scrollFunction()};
+    const title = document.querySelector('title').textContent;
 
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector("header").classList.add('scroll')
-    } else {
-        document.querySelector("header").classList.remove('scroll')
+
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+
+    if (title === 'Home') {
+
+        const welcomeSection = document.querySelector('.welcome-section');
+
+        document.body.style.overflow = 'hidden';
+
+        const goContainer = document.querySelector('.container-arrow');
+        const projectsSection = document.querySelector('.projects-section');
+
+        goContainer.addEventListener('click', () => {
+            document.body.style.overflow = 'visible';
+            welcomeSection.classList.add('scroll-up');
+
+
+        })
 
     }
-}
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.querySelector("header").classList.add('scroll')
+        } else {
+            document.querySelector("header").classList.remove('scroll')
+
+        }
+    }
+
+    function scrollToSection(target) {
+
+
+        const targetOffsetTop = target.offsetTop;
+
+        window.scrollTo({
+            top: targetOffsetTop,
+            behavior: 'smooth'
+        });
+    }
+
+
+});
+
+
